24ff6b967d63695a083e7df9c3f652b73c60ab89
mrmatteorusso <mrmatteorusso@gmail.com>
2023-04-14 14:38:42 +0200

    js dorpdown menu fixed

--- index.js
+++ index.js
@@ -2,41 +2,31 @@ const menu = document.querySelector(".menu");
 const menuItems = document.querySelectorAll(".menu-item");
 const hamburger = document.querySelector(".hamburger");
 const toggle = document.querySelector(".toggle-icon");
+const threeLineMenu = document.querySelector(".three-line-menu");
 const closeIcon = document.querySelector(".close-menu");
-const menuIcon = document.querySelector(".three-line-menu");
 
-function toggleMenu() {
+function toggleMenu(e) {
+  e.stopPropagation();
   if (menu.classList.contains("show-menu")) {
     menu.classList.remove("show-menu");
     closeIcon.style.display = "none";
-    menuIcon.style.display = "block";
+    threeLineMenu.style.display = "block";
   } else {
     menu.classList.add("show-menu");
     closeIcon.style.display = "block";
-    menuIcon.style.display = "none";
+    threeLineMenu.style.display = "none";
   }
-}
-
-function sayHi() {
-  console.log("blur");
-  alert("hi");
 }
 
 function hideMenu(e) {
-  if (
-    !menu.contains(e.target) &&
-    !toggle.contains(e.target) &&
-    !closeIcon.contains(e.target) &&
-    !menuIcon.contains(e.target)
-  ) {
-    toggleMenu();
+  if (e.target.classList[0] !== "three-line-menu") {
+    menu.classList.remove("show-menu");
+    closeIcon.style.display = "none";
+    threeLineMenu.style.display = "block";
   }
 }
 
+document.addEventListener("click", hideMenu);
 toggle.addEventListener("click", toggleMenu);
 
-menuItems.forEach(function (menuItem) {
-  menuItem.addEventListener("click", toggleMenu);
-});
-
-window.addEventListener("click", hideMenu);
+menuItems.forEach((menuItem) => menuItem.addEventListener("click", toggleMenu));
